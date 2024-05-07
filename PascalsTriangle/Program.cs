48b2993f3a97b3e3a12dc73dc64de3eba173a591
Vincent Bossert <vbossert17@gmail.com>
2024-05-07 09:18:52 +0200

    Second Commit

---
+++ PascalsTriangle/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+
+namespace PascalsTriangle
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int m_numrows)
+        {
+            var results = new List<IList<int>>();
+
+            if(m_numrows == 0)
+            {
+                return results;
+            }
+            results.Add(new List<int>(1){1});
+
+            if(m_numrows == 1)
+            {
+                return results;
+            }
+            results.Add(new List<int>(2){1, 1});
+            if(m_numrows == 2)
+            {
+                return results;
+            }
+
+            for(int i = 2; i < m_numrows; i++)
+            {
+                IList<int> result = new List<int>();
+                result.Add(1);
+
+                for(var j = 1; j < i; j++)
+                {
+                    var number = results[i - 1][j - 1] + results[i - 1][j];
+                    result.Add(number);
+                }
+                result.Add(1);
+                results.Add(result);
+            }
+
+            return results;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int numrows = 7;
+
+            IList<IList<int>> list = solution.Generate(numrows);
+            foreach(var innerlist in list)
+            {
+                foreach(var element in innerlist)
+                {
+                    Console.Write($" {element} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PathSumBinaryTree/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+
+namespace PathSumBinaryTree
+{
+    public class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class Solution
+    {
+
+        public TreeNode CreateTreeNode(int data)
+        {
+            TreeNode new_node = new TreeNode(data);
+            return new_node;
+        }
+
+        public bool hasPathToSum(TreeNode m_root, int m_targetsum)
+        {
+            return hasSumHelper(m_root, m_targetsum, 0);
+        }
+
+        public bool hasSumHelper(TreeNode helper_root, int helper_targetsum, int current_sum)
+        {
+            if(helper_root == null)
+            {
+                return false;
+            }
+
+            current_sum += helper_root.value;
+
+            if(current_sum == helper_targetsum)
+            {
+                return true;
+            }
+
+            return hasSumHelper(helper_root.left, helper_targetsum, current_sum) || hasSumHelper(helper_root.right, helper_targetsum, current_sum);   
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            TreeNode root = solution.CreateTreeNode(1);
+
+            root.left = solution.CreateTreeNode(2);
+            root.right = solution.CreateTreeNode(3);
+
+            root.right.right = solution.CreateTreeNode(5);
+            root.right.left = solution.CreateTreeNode(7);
+
+            root.left.left = solution.CreateTreeNode(4);
+            root.left.right = solution.CreateTreeNode(11);
+
+            root.left.left.left = solution.CreateTreeNode(8);
+
+            int target_sum = 15;
+
+            bool result = solution.hasPathToSum(root, target_sum);
+
+            Console.WriteLine($"Found the path to equal to target: {result.ToString()}");
+        }
+    }
+}
